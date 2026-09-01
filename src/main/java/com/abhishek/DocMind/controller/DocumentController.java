46212c2d3e277b8d01142c74f3de5983789336d5
abhishekkp00 <abhishekforcollege@gmail.com>
2026-09-01 21:16:58 +0530

    feat: Started with doc upload controller

--- src/main/java/com/abhishek/DocMind/controller/DocumentController.java
+++ src/main/java/com/abhishek/DocMind/controller/DocumentController.java
@@ -1,12 +1,19 @@
 package com.abhishek.DocMind.controller;
 
 
+import com.abhishek.DocMind.dto.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/vi/documents")
@@ -16,13 +23,27 @@ import org.springframework.web.bind.annotation.RestController;
 )
 public class DocumentController {
 
-    @PostMapping
+    private final DocumentMetadataService documentService;
+
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(
             summary = "Upload and index a document(PDF, DOCX, TEXT, MD, CSV)",
             description = "This api is used to upload and index documents files."
 
     )
-    public ResponseEntity<String> uploadDocuments(){
-        return ResponseEntity.ok("Document Uploaded.");
+    public ResponseEntity<ApiResponse<DocumentResponseDto>> uploadDocument(
+            @RequestParam("file") MultipartFile file
+    ) {
+
+
+//        process the files
+        DocumentResponseDto documentResponseDto = this.documentService.uploadAndProcess(file);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.<DocumentResponseDto>builder()
+                        .success(true)
+                        .data(documentResponseDto)
+                        .timestamp(LocalDateTime.now())
+                        .message("Documents uploaded and indexed successfully")
+                        .build());
     }
 }
